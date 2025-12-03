33a1fe5fea45cadd7c898245223dc516320ec9af
CODEX-TECH0 <codextech71@gmail.com>
2025-12-03 21:05:44 +0100

    updated

---
+++ script.js
@@ -0,0 +1,10 @@
+
+window.addEventListener("load", () => {
+  const preloader = document.getElementById("preloader");
+  preloader.style.opacity = "0";
+
+  setTimeout(() => {
+    preloader.style.display = "none";
+  }, 600); // match CSS fade time
+});
+
